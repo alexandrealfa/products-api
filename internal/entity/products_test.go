af6f3ac3e7cedb2914906f2204912d5bef44f68e
alexandrealfa <henriqueafcarvalho15@gmail.com>
2025-01-31 01:13:58 -0300

    (main) implements products Test

--- internal/entity/products_test.go
+++ internal/entity/products_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestNewProduct(t *testing.T) {
 	p, err := CreateProduct("ProductNew", 10)
+
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.Id)
@@ -16,6 +17,7 @@ func TestNewProduct(t *testing.T) {
 
 func TestWhenNameIsRequired(t *testing.T) {
 	p, err := CreateProduct("", 10)
+
 	assert.NotNil(t, err)
 	assert.Nil(t, p)
 	assert.Equal(t, errorNameIsEmpty, err)
@@ -23,6 +25,7 @@ func TestWhenNameIsRequired(t *testing.T) {
 
 func TestWhenPriceIsRequired(t *testing.T) {
 	p, err := CreateProduct("productValue", 0)
+
 	assert.NotNil(t, err)
 	assert.Nil(t, p)
 	assert.Equal(t, errorPriceRequired, err)
