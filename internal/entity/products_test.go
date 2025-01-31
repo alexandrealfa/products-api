1bc48fb47fb8314ce54fe9260148e11c29cba223
alexandrealfa <henriqueafcarvalho15@gmail.com>
2025-01-31 01:13:43 -0300

    (main) implements products Test

---
+++ internal/entity/products_test.go
@@ -0,0 +1,46 @@
+package entity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewProduct(t *testing.T) {
+	p, err := CreateProduct("ProductNew", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.Id)
+	assert.Equal(t, "ProductNew", p.Name)
+	assert.Equal(t, 10.0, p.Price)
+}
+
+func TestWhenNameIsRequired(t *testing.T) {
+	p, err := CreateProduct("", 10)
+	assert.NotNil(t, err)
+	assert.Nil(t, p)
+	assert.Equal(t, errorNameIsEmpty, err)
+}
+
+func TestWhenPriceIsRequired(t *testing.T) {
+	p, err := CreateProduct("productValue", 0)
+	assert.NotNil(t, err)
+	assert.Nil(t, p)
+	assert.Equal(t, errorPriceRequired, err)
+}
+
+func TestWhenPriceIsInvalid(t *testing.T) {
+	p, err := CreateProduct("product Invalid", -10)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, p)
+	assert.Equal(t, errorInvalidPrice, err)
+}
+
+func TestProductValidate(t *testing.T) {
+	p, err := CreateProduct("validProduct", 10)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.Equal(t, p.Name, "validProduct")
+	assert.Nil(t, p.Validate())
+}
