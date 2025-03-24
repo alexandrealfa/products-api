7cee56d101cd62744399c92312f2059ac648d4bb
alexandrealfa <henriqueafcarvalho15@gmail.com>
2025-03-24 01:03:57 -0300

    (main) implements dto package

---
+++ internal/dto/dto.go
@@ -0,0 +1,17 @@
+package dto
+
+type CreateProductDTO struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type UpdateProductDTO struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type CreateUserDTO struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
